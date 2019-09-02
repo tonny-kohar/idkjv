b007ee30c8116b84cac93369b0af9a1cf058e1f2
Tonny <none@none>
2019-09-02 18:38:00 +0700

    luk 10.30

--- make-sword.bat
+++ make-sword.bat
@@ -9,8 +9,8 @@ set OUTPUTPATH=sword\modules\texts\ztext\idkjv\
 REM rm -f $OUTPUTPATH/*.*
 
 ECHO "40-MAT.xml"
-osis2mod %OUTPUTPATH% 40-MAT.xml -z -b 4 -v KJV
+D:\osis2mod\osis2mod %OUTPUTPATH% 40-MAT.xml -z -b 4 -v KJV
 ECHO "41-MRK.xml"
-osis2mod %OUTPUTPATH% 41-MRK.xml -a -z -b 4 -v KJV
+D:\osis2mod\osis2mod %OUTPUTPATH% 41-MRK.xml -a -z -b 4 -v KJV
 ECHO "42-Luk.xml"
-osis2mod %OUTPUTPATH% 42-LUK.xml -a -z -b 4 -v KJV
+D:\osis2mod\osis2mod %OUTPUTPATH% 42-LUK.xml -a -z -b 4 -v KJV
