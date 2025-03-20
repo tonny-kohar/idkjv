1c2e44f2943d115c1db3d09cf33f4ca6fb1b5c56
Tonny Kohar <13114803+tonny-kohar@users.noreply.github.com>
2025-03-20 18:22:34 +0700

    acts.16.34

--- make-sword.bat
+++ make-sword.bat
@@ -6,8 +6,8 @@ copy idkjv.conf sword\mods.d\
 
 set OUTPUTPATH=sword\modules\texts\ztext\idkjv\
 
-D:\osis2mod\osis2mod %OUTPUTPATH% 40-MAT.xml -z -b 4 -v KJV
-D:\osis2mod\osis2mod %OUTPUTPATH% 41-MRK.xml -a -z -b 4 -v KJV
-D:\osis2mod\osis2mod %OUTPUTPATH% 42-LUK.xml -a -z -b 4 -v KJV
-D:\osis2mod\osis2mod %OUTPUTPATH% 43-JOHN.xml -a -z -b 4 -v KJV
-D:\osis2mod\osis2mod %OUTPUTPATH% 44-ACTS.xml -a -z -b 4 -v KJV
+..\osis2mod\osis2mod %OUTPUTPATH% 40-MAT.xml -z -b 4 -v KJV
+..\osis2mod\osis2mod %OUTPUTPATH% 41-MRK.xml -a -z -b 4 -v KJV
+..\osis2mod\osis2mod %OUTPUTPATH% 42-LUK.xml -a -z -b 4 -v KJV
+..\osis2mod\osis2mod %OUTPUTPATH% 43-JOHN.xml -a -z -b 4 -v KJV
+..\osis2mod\osis2mod %OUTPUTPATH% 44-ACTS.xml -a -z -b 4 -v KJV
